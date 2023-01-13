85ae1b74a8a4f35809a453489500d952a5b58757
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-01-13 17:53:00 +0100

    Add navigation bar (#3)
    * Cleanup
    
    * Install TailwindCSS
    
    * First design attempt
    
    * Navigation bar
    
    * Add title updating
    
    * Clean up navbar

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,91 +1,58 @@
 <script setup lang="ts">
+import { toNumber } from "@vue/shared";
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<script lang="ts">
+export default {
+  name: "App",
+  watch: {
+    $route: {
+      immediate: true,
+      handler(to, _) {
+        document.title = "MTAS";
+
+        if (to.name !== undefined && to.name !== null) {
+          let routeName = to.name;
+          let title = routeName.charAt(0).toUpperCase() + routeName.slice(1);
+          document.title = `MTAS - ${title}`;
+        }
+      },
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div class="container mx-auto m-4">
+    <nav>
+      <div class="flex flex-wrap items-center justify-between px-8">
+        <img
+          src="./assets/images/example-logo.png"
+          class="h-8 md:h-10 mr-3"
+          alt="MTAS logo"
+        />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        <div class="w-auto space-x-8 md:space-x-16 navbar">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/tools">Tools</RouterLink>
+          <a href="https://github.com/RooTender/MTAS" target="_blank">Github</a>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.navbar > *:hover {
+  text-decoration: underline;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.navbar > a.router-link-exact-active {
+  font-weight: bold;
 }
 </style>
