94e8f13967d833c0c650a88d8cd0e16f2f314e9c
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-01-13 13:57:46 +0100

    First design attempt

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -3,11 +3,32 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </header>
-
-  <RouterView />
+  <div class="m-4">
+    <header>
+      <img
+        class="h-16 md:h-20 lg:h-24 mx-auto"
+        alt="logo"
+        src="./assets/images/example-logo.png"
+      />
+    </header>
+    <nav
+      class="my-4 flex flex-row justify-center space-x-4 md:space-x-8 lg:space-x-16 navbar"
+    >
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar > *:hover {
+  text-decoration: underline;
+}
+
+.navbar > a.router-link-exact-active {
+  font-weight: bold;
+}
+</style>
