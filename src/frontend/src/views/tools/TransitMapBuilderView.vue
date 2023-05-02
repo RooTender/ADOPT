1471528e1e585cb5d04da053262bf55291bc1c20
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-05-02 15:26:58 +0200

    Add form validation

--- src/frontend/src/views/tools/TransitMapBuilderView.vue
+++ src/frontend/src/views/tools/TransitMapBuilderView.vue
@@ -8,14 +8,42 @@ export default {
 }
 </script>
 
+<script setup lang="ts">
+
+interface InputFormData extends FormData {
+  data: {
+    name: string;
+    file: File;
+  }[];
+}
+
+const submitHandler = async (file: InputFormData) => {
+  const formData = new FormData();
+  formData.append('name', file.data[0].name);
+  formData.append('data', file.data[0].file);
+
+  const allowedExtensions = /(\.csv)$/i;
+
+  if (!allowedExtensions.exec(file.data[0].name)) {
+    alert('Please select a .csv file.');
+  }
+  else
+  {
+    console.log("All good");
+  }
+}
+</script>
+
 <template>
   <StatusBar :step="1" :steps="['Graph to analyze', 'Processing', 'Results']"/>
 
   <div class="lg:mx-20">
-    <FormKit type="form">
+    <FormKit type="form" @submit="submitHandler">
       <FormKit
         type="file"
+        name="data"
         validation="required|not:Admin"
+        :validation-messages="{ required: 'Please select a .csv file.' }"
         label="Bus stop data"
         multiple="false"
         accept=".csv"
