a567bb4081f05fc49ed7a54caf11f0e14fc7a2c4
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-07-14 23:36:08 +0200

    Refactor and optimize tests
    - removed unused library (@testing-library/vue)
    - updated timeout from 200 to 1000 ms
    - refactored code

--- src/frontend/test/views/App.test.ts
+++ src/frontend/test/views/App.test.ts
@@ -1,100 +1,106 @@
-import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest';
-import router from "../../src/router/index"
-import App from '../../src/App.vue';
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import router from "../../src/router/index";
+import App from "../../src/App.vue";
 
-const routerTest = async (test: any) => {
+const testRenderedElement = async (test: any) => {
   setTimeout(() => {
     test();
-  }, 200);
+  }, 1000);
 };
 
-describe('App.vue', () => {
-    it('sets the document title to "MTAS" initially', () => {
+describe("App.vue", () => {
+  it('sets the document title to "MTAS" initially', () => {
     mount(App, {
-    global: {
-        plugins: [router]
-        }
+      global: {
+        plugins: [router],
+      },
+    });
+
+    expect(document.title).toEqual("MTAS");
   });
 
-  expect(document.title).toEqual('MTAS');
-});
-
-it('updates the document title based on the route name', async () => {
+  it("updates the document title based on the route name", async () => {
     mount(App, {
-    global: {
-        plugins: [router]
-    }
+      global: {
+        plugins: [router],
+      },
     });
-    router.push({ name: 'about' });
+    router.push({ name: "about" });
 
-    routerTest(() => {
-        expect(document.title).toEqual('MTAS - About');
+    testRenderedElement(() => {
+      expect(document.title).toEqual("MTAS - About");
     });
-});
+  });
 
-it('renders the correct navigation links', async () => {
+  it("renders the correct navigation links", async () => {
     const wrapper = mount(App, {
-    global: {
-        plugins: [router]
-    }
+      global: {
+        plugins: [router],
+      },
     });
 
-    routerTest(() => {
-      const routerLinks = wrapper.findAll('a.router-link');
+    testRenderedElement(() => {
+      const routerLinks = wrapper.findAll("a.router-link");
 
       expect(routerLinks.length).toBe(4);
-      expect(routerLinks[0].text()).toBe('Home');
-      expect(routerLinks[1].text()).toBe('About');
-      expect(routerLinks[2].text()).toBe('Tools');
-      expect(routerLinks[3].attributes('href')).toBe('https://github.com/RooTender/MTAS');
+      expect(routerLinks[0].text()).toBe("Home");
+      expect(routerLinks[1].text()).toBe("About");
+      expect(routerLinks[2].text()).toBe("Tools");
+      expect(routerLinks[3].attributes("href")).toBe(
+        "https://github.com/RooTender/MTAS"
+      );
     });
   });
 
-  it('applies styles to active router link', () => {
+  it("applies styles to active router link", async () => {
     const wrapper = mount(App, {
       global: {
-        plugins: [router]
-      }
+        plugins: [router],
+      },
     });
 
-    routerTest(() => {
-      const activeLink = wrapper.find('a.router-link-exact-active');
+    testRenderedElement(() => {
+      const activeLink = wrapper.find("a.router-link-exact-active");
 
-      expect(activeLink.classes()).toContain('router-link-exact-active');
-      expect(activeLink.attributes('style')).toContain('font-weight: bold;');
+      expect(activeLink.classes()).toContain("router-link-exact-active");
+      expect(activeLink.attributes("style")).toContain("font-weight: bold;");
     });
   });
 
-  it('applies bold font-weight to active router link', () => {
+  it("applies bold font-weight to active router link", async () => {
     const wrapper = mount(App, {
       global: {
-        plugins: [router]
-      }
+        plugins: [router],
+      },
     });
 
-    routerTest(() => {
-      const activeLink = wrapper.find('a.router-link-exact-active');
+    testRenderedElement(() => {
+      const activeLink = wrapper.find("a.router-link-exact-active");
 
-      expect(activeLink.attributes('style')).toContain('font-weight: bold;');
+      expect(activeLink.attributes("style")).toContain("font-weight: bold;");
     });
   });
 
-  it('underlines router link on hover', () => {
+  it("underlines router link on hover", async () => {
     const wrapper = mount(App, {
-    global: {
-        plugins: [router]
-    }
+      global: {
+        plugins: [router],
+      },
     });
 
-    routerTest(() => {
-      const activeLink = wrapper.find('a.router-link-exact-active');
+    testRenderedElement(() => {
+      const activeLink = wrapper.find("a.router-link-exact-active");
 
-      activeLink.trigger('mouseover');
-      expect(activeLink.attributes('style')).toContain('text-decoration: underline;');
+      activeLink.trigger("mouseover");
+      expect(activeLink.attributes("style")).toContain(
+        "text-decoration: underline;"
+      );
 
-      activeLink.trigger('mouseout');
-      expect(activeLink.attributes('style')).not.toContain('text-decoration: underline;');
+      activeLink.trigger("mouseout");
+      expect(activeLink.attributes("style")).not.toContain(
+        "text-decoration: underline;"
+      );
     });
   });
 });
