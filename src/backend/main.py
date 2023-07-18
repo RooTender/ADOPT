ae1aa218b98ecf0f98cf1ee1cd4bdc7b83b191a9
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-07-19 01:19:51 +0200

    Add rabbitmq to backend (WIP)

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import pika
 
 app = FastAPI()
 
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='xd')
+
 @app.get('/')
 async def test():
+    channel.basic_publish(exchange='',
+                      routing_key='xd',
+                      body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
     return {"message": "Hello world!"}
+
+connection.close()
