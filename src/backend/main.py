ee0c682db6359767142e952dedcdb30f0e9c5e97
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-07-19 18:27:33 +0200

    Fix backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -5,14 +5,13 @@ app = FastAPI()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('broker'))
 channel = connection.channel()
-channel.queue_declare(queue='xd')
+channel.queue_declare(queue='hello')
+
 
 @app.get('/')
 async def test():
     channel.basic_publish(exchange='',
-                      routing_key='xd',
-                      body='Hello World!')
+                          routing_key='hello',
+                          body='Hello World!')
     print(" [x] Sent 'Hello World!'")
     return {"message": "Hello world!"}
-
-connection.close()
